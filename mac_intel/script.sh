6c861a80b9aeefaae162be61140968f4bc96c7c4
kaczmarczykmatthew <104938930+kaczmarczykmatthew@users.noreply.github.com>
2022-06-02 13:11:58 +0200

    Update script.sh

--- mac_intel/script.sh
+++ mac_intel/script.sh
@@ -34,7 +34,7 @@ echo""
 echo""
 echo "********************* Executing Ansible *********************"
 echo""
-	ansible-playbook install.yml -vv
+	ansible-playbook install.yml -v
 echo""
 echo "************************************************"
 echo "************************************************"
