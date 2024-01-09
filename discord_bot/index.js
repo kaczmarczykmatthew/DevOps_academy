ce92affca39387763484d661c712977cfc6b2a47
mateusz <mtkaczmarczyk@gmail.com>
2024-01-09 19:44:25 +0000

    modifed index.js

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -25,22 +25,24 @@ client.on('messageCreate', message => {
 	message.reply('test');
 	        	}
 })
-// Create a console output from any message
+//Create a console output from any message
 //client.on('messageCreate', message => {
 //	console.log(message);
 
 
 
 //})
-
-
-// Log in to Discord with your client's token
-
-client.on('interactionCreate', interaction => {
+// Create /help and /user <username> query
+client.on('interactionCreate', (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 	if (interaction.commandName === 'help') {
-		interaction.reply('```Use /user to check when user was last in the voice channel```');
+		interaction.reply('```1. Use /user <username> to check when user was last in the voice channel \n2. To Do\n3. To Do\n4. To Do\n5. To Do```');
+	};
+	if (!interaction.isChatInputCommand()) return;
+	if (interaction.commandName === 'user') {
+		const userek = interaction.options.get('username').value;
+		interaction.reply(`The name is ${userek}`)
 	};
 });
-
+// Log in to Discord with your client's token
 client.login(process.env.TOKEN);
